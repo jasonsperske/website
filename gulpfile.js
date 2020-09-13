40ee7ac0232daf0ab9620fa85c7b26ca9f8f896d
Jason Sperske <jason@sperske.com>
2020-09-13 01:43:09 -0700

    Updaed more deps and added lightbox
    also fixed some alignment bugs in the layout files.

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,8 @@ function javascript() {
   return gulp.src([
       'node_modules/jquery/dist/jquery.js',
       'node_modules/popper.js/dist/umd/popper.js',
-      'node_modules/bootstrap/dist/js/bootstrap.js'
+      'node_modules/bootstrap/dist/js/bootstrap.js',
+      'node_modules/lity/dist/lity.js'
     ], {
       base: '.'
     })
